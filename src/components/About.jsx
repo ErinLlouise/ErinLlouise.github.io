ef1a31152070df8d6b84724aec9c984b6e20bea7
Erin <erinllouise3@gmail.com>
2021-10-11 10:48:43 +1300

    layout about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,45 @@ import './About.styles.css'
 function About () {
     return (
         <>
-          <div className='about'>
-            <h1>About</h1>
+          <div>
+            <header>
+              <div class="container container_solid">
+                <div class="title_wrapper">
+                  <h1>About</h1>
+                </div>
+              </div>
+              <div class="container container_image" aria-hidden="true">
+                <div class="title_wrapper">
+                  <h1>About</h1>
+                  {/* <h2>I am a Creative Technologist and Software Developer 
+                      with a deep passion for designing, creating and building 
+                      interactive experiences.</h2> */}
+                </div>
+              </div>
+            </header>
+            <div className='sectionC'>
+              <section>
+                <h1>Erin Louise Abernethy.</h1>
+                <p>I am a Creative Technologist and Software Developer 
+                   with a deep passion for designing, creating and building 
+                   interactive experiences. I am always excited by new and 
+                   evolving tech and am personally driven by a limitless 
+                   curiosity and eagerness to learn.
+                </p>
+                <p>My career goal is to have an influence on the tech sector 
+                   in Aotearoa, and contribute to our country's success in a 
+                   post Covid-19 world.</p>
+                <div className='icons'>
+                  <img src='mail.png' alt='click to copy email' />
+                  <img src='person.png' alt='click to follow Erin on Linked In' />
+                  <img src='download.png' alt='click to download erins cv' />
+                </div>
+              </section>
+            </div>
+          <div className='arrowpos'>
+            <img className='arrow' src='warrow.png' alt='down arrow' />
           </div>
+        </div>
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,9 +4,36 @@ import './Projects.styles.css'
 function Projects () {
     return (
         <>
-          <div className='projects'>
-            <h1>Projects</h1>
+          <div>
+            <header>
+              <div class="container container_solid">
+                <div class="title_wrapper">
+                  <h1>Projects</h1>
+                </div>
+              </div>
+              <div class="container container_image" aria-hidden="true">
+                <div class="title_wrapper">
+                  <h1>Projects</h1>
+                </div>
+              </div>
+              </header>
+            <div className='sectionC'>
+              <section>
+                <h2>Lorem ipsum dolor sit amet.</h2>
+                <p>Consectetur adipiscing elit, sed do eiusmod tempor 
+                  incididunt ut labore et dolore magna aliqua. Ut enim 
+                  ad minim veniam, quis nostrud exercitation ullamco 
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis 
+                  aute irure dolor in reprehenderit in voluptate velit 
+                  esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                  sint occaecat cupidatat non proident, sunt in culpa qui 
+                  officia deserunt mollit anim id est laborum.</p>
+              </section>
+            </div>
+          <div className='arrowpos'>
+            <img className='arrow' src='warrow.png' alt='down arrow' />
           </div>
+        </div>
         </>
     )
 }
