8fa000a79ae05f787e35d8b57be92a575f96ea34
Erin <erinllouise3@gmail.com>
2021-10-12 10:29:40 +1300

    create timeline comp

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,53 @@
+import './App.css';
+
+function Timeline () {
+  return (
+    <div >
+      <ul class="timeline">
+<li>
+  <div class="direction-r">
+    <div class="flag-wrapper">
+      <span class="flag">Accenture Interactive</span>
+      <span class="time-wrapper"><span class="time">2021 - present</span></span>
+    </div>
+    <div class="desc">My current employment as a Front End Development Analyst</div>
+  </div>
+</li>
+
+<li>
+  <div class="direction-l">
+    <div class="flag-wrapper">
+      <span class="flag">Enspiral Dev Academy</span>
+      <span class="time-wrapper"><span class="time">Feb - May</span></span>
+    </div>
+    <div class="desc">Full stack web development boot camp.</div>
+  </div>
+</li>
+
+<li>
+  <div class="direction-r">
+    <div class="flag-wrapper">
+      <span class="flag">Bachelors Degree</span>
+      <span class="time-wrapper"><span class="time">2018 - 2020</span></span>
+    </div>
+    <div class="desc">Bachelor of Creative Technologies</div>
+  </div>
+</li>
+<li>
+  <div class="direction-l">
+    <div class="flag-wrapper">
+      <span class="flag">Mecca Brands</span>
+      <span class="time-wrapper"><span class="time">2018-2021</span></span>
+    </div>
+    <div class="desc">Operations Specialst, Beauty Lab Assistant, Store Host.</div>
+  </div>
+</li>
+
+
+</ul>
+
+    </div>
+  );
+}
+
+export default Timeline;
