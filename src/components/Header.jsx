7286e49397a9cc22101823407466ff7e4a21fcc6
Erin <erinllouise3@gmail.com>
2021-11-17 11:57:10 +1300

    add responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,19 @@ import './Header.css'
 function Header () {
     return (
       <>
-        <div className="header">
-          
-        </div>
+        <header>
+          <div class="header">
+            <div class="logo">LOGO</div>
+            <label for="toggle" class="toggleBtn">menu</label>
+            <input type="checkbox" id="toggle" />
+            <ul class="navbar">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Services</a></li>
+            </ul>
+          </div>
+        </header>
       </>
     )
 }
