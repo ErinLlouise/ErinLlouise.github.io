30400ce3ec1210152316e0703ceabfc88f267e70
Erin <erinllouise3@gmail.com>
2021-11-17 15:42:12 +1300

    add animation to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import ProjectsButton from './buttons/ProjectsButton'
-import AboutButton from './buttons/AboutButton'
-import HomeButton from './buttons/HomeButton'
+import React, { useEffect, useRef }from 'react'
+// import ProjectsButton from './buttons/ProjectsButton'
+// import AboutButton from './buttons/AboutButton'
+// import HomeButton from './buttons/HomeButton'
 import './Header.css'
+import { gsap } from "gsap";
+import { Link } from 'react-router-dom';
+
 
 function Header () {
+  // store a reference to the box div
+  const boxRef = useRef();
+
+  // wait until DOM has been rendered
+  useEffect(() => {
+    gsap.to(boxRef.current, {rotation:"+=360", repeat:-1, duration: 15, ease: "none"});
+  });
+
     return (
       <>
         <header>
           <div class="header">
             <div class="logoContainer">
-              <img class="logo" src="logo.png"/>
+              <img ref={boxRef} class="logo" src="logo.png" alt="logo"/>
             </div>
-            <label for="toggle" class="toggleBtn">menu</label>
+            {/* hamburger icon */}
+            <label for="toggle" class="toggleBtn">
+              <div className="icon"></div>
+              <div className="icon"></div>
+              <div className="icon"></div>
+            </label>
             <input type="checkbox" id="toggle" />
             <ul class="navbar">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Projects</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
             </ul>
           </div>
         </header>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,55 +1,11 @@
 import React from 'react' 
-import { HashLink as Link } from 'react-router-hash-link'
-import Arrow from '@material-ui/icons/ExpandMore'
-import ArrowUp from '@material-ui/icons/ExpandLess'
 import './Projects.css'
 
 function Projects () {
     return (
         <>
        <div>
-        <header>
-          <div id='sectionA'>
-            <div class="container container_solid">
-              <div class="title_wrapper">
-                <h1>Projects</h1>
-              </div>
-            </div>
-            <div>
-              <Link to='#sectionB'>
-                <Arrow style={{ color: "#ff4219", "fontSize" : "50px" }} className='arrow bounce bounce-7' src='warrow.png'/> 
-              </Link>
-            </div>
-          </div>
-            <div id='sectionB' class="container container_image" aria-hidden="true">
-              <div class="title_wrapper">
-                <h1 >Projects</h1>
-              </div>
-              <div>
-                <Link to='#sectionC'>
-                  <Arrow style={{ color: "#ffbdb3", "fontSize" : "50px" }} className='arrow bounce bounce-7' src='warrow.png'/> 
-                </Link>
-              </div>
-            </div>
-          </header>
-          <div>
-            <section>
-              <h1 id='sectionC' className='head'>Welcome to my portfolio.</h1>
-              <p>Consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim 
-                ad minim veniam, quis nostrud exercitation ullamco 
-                laboris nisi ut aliquip ex ea commodo consequat. Duis 
-                aute irure dolor in reprehenderit in voluptate velit 
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                sint occaecat cupidatat non proident, sunt in culpa qui 
-                officia deserunt mollit anim id est laborum.</p>
-                <div>
-                <Link to='#sectionA'>
-                  <ArrowUp style={{ color: "#ff4219", "fontSize" : "50px" }} className='arrowup bounce bounce-7' src='warrow.png'/> 
-                </Link>
-              </div>
-            </section>
-          </div>
+       
         </div>
         </>
     )
